3943c1df28a12fc840fe0f13e93dec07427f5b91
Regule <regule@zoho.com>
2021-01-18 11:43:24 +0100

    Started to work on controller test node

--- src/OculiBase/OculiBase/snes_test.py
+++ src/OculiBase/OculiBase/snes_test.py
@@ -1,5 +1,40 @@
-def main():
-    print('Hi from OculiBase.')
+import rclpy
+import pygame as pg
+from rclpy.node import Node
+
+
+def select_controller():
+    pg.joystick.init()
+    controllers = [pg.joystick.Joystick(x) for x in range(pg.joystick.get_count())]
+    if len(controllers) == 0:
+        print('No controller connected.')
+        return None
+    for idx, controller in enumerate(controllers):
+        print(f'{idx} ---> {controller.get_name()}')
+    choice = int(input('Select controller >'))
+    try:
+        controllers[choice].init()
+        return controllers[choice]
+    except IndexError:
+        print(f'No controller at id {choice}')
+        return None
+
+
+class SnesController:
+
+    def __init__(self, node_name, controller, update_period):
+        super().__init__(node_name)
+        self.controller = controller
+        self.timer = self.create_timer(update_period, self.update)
+
+    def update():
+        pass
+
+
+def main(args=None):
+    controller = select_controller()
+    if controller is None:
+        return
 
 
 if __name__ == '__main__':
